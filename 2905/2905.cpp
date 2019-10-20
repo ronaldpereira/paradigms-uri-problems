fbc21e26b3c3d236612bf9a3366591cc5a493873
Ronald <ronald.drp11@gmail.com>
2019-10-20 14:27:47 -0300

    input reading

--- 2905/2905.cpp
+++ 2905/2905.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
     // Fast reading from input
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
+
+    int N;
+    std::cin >> N;
+
+    std::vector<int> durations, costs;
+
+    for (int i = 0; i < N; i++)
+    {
+        int duration, cost;
+        std::cin >> duration >> cost;
+        durations.push_back(duration);
+        costs.push_back(cost);
+    }
 }
