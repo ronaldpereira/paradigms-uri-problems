ef3a987f7f15ccf064dbdacfdb0a1525b51b30a6
Ronald <ronald.drp11@gmail.com>
2019-10-20 00:27:25 -0300

    updated code and added vscode to gitignore

--- 2795/2795.cpp
+++ 2795/2795.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <algorithm>
 
 bool IsPalindrome(std::string segment)
 {
@@ -18,10 +19,38 @@ bool IsPalindrome(std::string segment)
     }
 }
 
-void GenerateSegments(int N, int K, std::string chain_chars)
+int GetChangeCost(char letter_before, char letter_after)
 {
     std::array<char, 26> alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                                      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    auto iterator_letter_before = std::find(alphabet.begin(), alphabet.end(), letter_before);
+    auto iterator_letter_after = std::find(alphabet.begin(), alphabet.end(), letter_after);
+
+    int position_before = iterator_letter_before - alphabet.begin() + 1;
+    int position_after = iterator_letter_after - alphabet.begin() + 1;
+
+    if (iterator_letter_before <= iterator_letter_after)
+    {
+        if ((position_after - position_before) <= (position_before - position_after))
+            return position_after - position_before;
+        else
+            return position_before - position_after;
+    }
+
+    // Done a loop over the end of alphabet
+    else
+    {
+        if ((position_after - position_before) <= (position_before - position_after))
+            return alphabet.size() - position_before + position_after;
+
+        else
+            return alphabet.size() - position_after + position_before;
+    }
+}
+
+void GenerateSegments(int N, int K, std::string chain_chars)
+{
     int actual_K = 0;
 
     if (N == K)
@@ -37,10 +66,12 @@ int main()
     std::string chain_chars;
     int N, K;
 
-    std::cin >> N >> K;
-    std::cin >> chain_chars;
+    // std::cin >> N >> K;
+    // std::cin >> chain_chars;
 
-    GenerateSegments(N, K, chain_chars);
+    // GenerateSegments(N, K, chain_chars);
+
+    std::cout << GetChangeCost('a', 'c') << " " << GetChangeCost('a', 'z') << std::endl;
 
     return 0;
 }
