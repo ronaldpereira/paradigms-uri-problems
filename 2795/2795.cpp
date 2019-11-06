099ebae76726df7fb77118f8b16a8f0556d5506b
Ronald <ronald.drp11@gmail.com>
2019-11-06 20:52:04 -0300

    palidromes version updated

--- 2795/2795.cpp
+++ 2795/2795.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <limits.h>
 
-int dp[400][400];
-char word[400];
-int change_cost_dp[26][26];
+std::string word;
+std::vector<std::vector<int>> change_cost_dp(26, std::vector<int>(26));
 
-int PalindromeCost(int K, int index_begin, int index_end)
+int GetChangeCost(int index_begin, int index_end)
 {
-    if (index_begin == index_end)
-        return 0;
+    int cost = 0;
+    for (int i = index_begin, j = index_end; i < j; i++, j--)
+        cost += change_cost_dp[(int)word[i] - 'a'][(int)word[j] - 'a'];
 
-    if (K == 1)
-    {
-        if (dp[index_begin][index_end] == -1)
-        {
-            int cost = 0;
-            for (int i = index_begin, j = index_end; i < j; i++, j--)
-            {
-                if (word[i] != word[j])
-                    cost += change_cost_dp[(int)word[i] - 'a'][(int)word[j] - 'a'];
-            }
-            dp[index_begin][index_end] = cost;
-        }
-        return dp[index_begin][index_end];
-    }
+    return cost;
+}
+
+int PalindromeCost(int K, int i)
+{
+    if (K == 1 || i == word.length() - 1)
+        return GetChangeCost(i, word.length() - 1);
 
     else
     {
-        int cost = PalindromeCost(1, index_begin, index_end);
-        for (int i = index_begin; i < index_end; i++)
+        int cost = PalindromeCost(1, 0);
+        for (int j = i; j < word.length() - 1; j++)
         {
-            int left = PalindromeCost(1, index_begin, i);
-            int right = PalindromeCost(K - 1, i + 1, index_end);
+            int left = GetChangeCost(i, j);
+            int right = PalindromeCost(K - 1, j + 1);
 
             cost = std::min(cost, left + right);
         }
-        dp[index_begin][index_end] = cost;
 
-        return dp[index_begin][index_end];
+        return cost;
     }
 }
 
@@ -51,10 +46,6 @@ int main()
     std::cin >> N >> K;
     std::cin >> word;
 
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            (i == j) ? dp[i][j] = 0 : dp[i][j] = -1;
-
     for (int i = 0; i < 26; i++)
         for (int j = 0; j < 26; j++)
             change_cost_dp[i][j] = std::min(std::abs(j - i), 26 - std::abs(j - i));
@@ -63,7 +54,7 @@ int main()
         std::cout << 0 << std::endl;
 
     else
-        std::cout << PalindromeCost(K, 0, N - 1) << std::endl;
+        std::cout << PalindromeCost(K, 0) << std::endl;
 
     return 0;
 }
