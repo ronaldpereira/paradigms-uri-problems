7089a6fedc26a6716495ce45b67c46841a37d13f
Ronald <ronald.drp11@gmail.com>
2019-11-09 10:54:45 -0300

    updated 2795 and 2942 (still TLE)

--- 2795/2795.cpp
+++ 2795/2795.cpp
@@ -4,6 +4,7 @@
 #include <limits.h>
 
 std::string word;
+std::vector<std::vector<int>> dp;
 std::vector<std::vector<int>> change_cost_dp(26, std::vector<int>(26));
 
 int GetChangeCost(int index_begin, int index_end)
@@ -18,14 +19,18 @@ int GetChangeCost(int index_begin, int index_end)
 int PalindromeCost(int K, int i)
 {
     if (K == 1 || i == word.length() - 1)
-        return GetChangeCost(i, word.length() - 1);
+    {
+        dp[i][word.length() - 1] = GetChangeCost(i, word.length() - 1);
+        dp[word.length() - 1][i] = dp[i][word.length() - 1];
+        return dp[i][word.length() - 1];
+    }
 
     else
     {
         int cost = PalindromeCost(1, 0);
         for (int j = i; j < word.length() - 1; j++)
         {
-            int left = GetChangeCost(i, j);
+            int left = (dp[i][j] != -1) ? dp[i][j] : GetChangeCost(i, j);
             int right = PalindromeCost(K - 1, j + 1);
 
             cost = std::min(cost, left + right);
@@ -49,6 +54,11 @@ int main()
     for (int i = 0; i < 26; i++)
         for (int j = 0; j < 26; j++)
             change_cost_dp[i][j] = std::min(std::abs(j - i), 26 - std::abs(j - i));
+
+    dp = std::vector<std::vector<int>>(N, std::vector<int>(N));
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            dp[i][j] = (i == j) ? 0 : -1;
 
     if (N == K)
         std::cout << 0 << std::endl;
--- 2942/2942.cpp
+++ 2942/2942.cpp
@@ -3,27 +3,47 @@
 #include <algorithm>
 #include <set>
 
-long long ShellSortAndReturnMovements(std::vector<int> permutation, int N)
+std::vector<int> auxiliar;
+std::vector<int> xor_indexes;
+
+long long MergeSort(int low, int high)
 {
-    long long movements = 0LL;
+    if (low >= high)
+        return 0LL;
 
-    for (int gap = N / 2; gap > 0; gap /= 2)
+    int mid = (low + high) / 2;
+    long long low_mid = 0LL;
+    long long mid_high = 0LL;
+    low_mid = MergeSort(low, mid);
+    mid_high = MergeSort(mid + 1, high);
+
+    long long inversions = 0LL;
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid and j <= high)
     {
-        for (int i = gap; i < N; i++)
-        {
-            int temp = permutation[i];
+        if (xor_indexes[i] <= xor_indexes[j])
+            auxiliar[k++] = xor_indexes[i++];
 
-            int j;
-            for (j = i; j >= gap && permutation[j - gap] > temp; j -= gap)
-            {
-                permutation[j] = permutation[j - gap];
-                movements += gap + gap - 1;
-            }
-            permutation[j] = temp;
+        else
+        {
+            auxiliar[k++] = xor_indexes[j++];
+            inversions += std::abs(mid + 1 - i);
         }
     }
 
-    return movements;
+    while (i <= mid)
+        auxiliar[k++] = xor_indexes[i++];
+
+    while (j <= high)
+        auxiliar[k++] = xor_indexes[j++];
+
+    for (int i = low; i <= high; i++)
+        xor_indexes[i] = auxiliar[i];
+
+    return inversions + low_mid + mid_high;
 }
 
 long long GetMinXOROperations(std::vector<int> before, std::vector<int> after, int N)
@@ -37,53 +57,49 @@ long long GetMinXOROperations(std::vector<int> before, std::vector<int> after, i
     for (int i = 0; i < N - 1; i++)
         xor_after.push_back(after[i] ^ after[i + 1]);
 
-    if (std::multiset<int>(xor_before.begin(), xor_before.end()) != std::multiset<int>(xor_after.begin(), xor_after.end()))
+    if (before[0] != after[0] || before[N - 1] != after[N - 1])
+        return -1;
+
+    else if (std::multiset<int>(xor_before.begin(), xor_before.end()) != std::multiset<int>(xor_after.begin(), xor_after.end()))
         return -1;
 
     else
     {
-        std::vector<int> xor_indexes(N - 1);
+        xor_indexes = std::vector<int>(N - 1);
+        auxiliar = std::vector<int>(N - 1);
         for (int i = 0; i < N - 1; i++)
         {
-            auto it = std::find(xor_after.begin(), xor_after.end(), xor_before[i]);
-            int position = it - xor_after.begin();
+            auto it = std::find(xor_before.begin(), xor_before.end(), xor_after[i]);
+            int position = it - xor_before.begin();
 
-            xor_indexes[i] = position;
-            xor_before[i] = -1;
-            xor_after[position] = -1;
+            xor_indexes[position] = i;
+            xor_after[i] = -1;
+            xor_before[position] = -1;
         }
-        return ShellSortAndReturnMovements(xor_indexes, N - 1);
+        return MergeSort(0, N - 2);
     }
 }
 
 int main()
 {
-    // Fast reading from input
+    // Fast reading from xor_indexes
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
     int N;
     std::cin >> N;
 
-    std::vector<int> vector_before, vector_after;
+    std::vector<int> vector_before(N), vector_after(N);
 
     for (int i = 0; i < N; i++)
-    {
-        int number;
-        std::cin >> number;
-        vector_before.push_back(number);
-    }
+        std::cin >> vector_before[i];
 
     for (int i = 0; i < N; i++)
-    {
-        int number;
-        std::cin >> number;
-        vector_after.push_back(number);
-    }
+        std::cin >> vector_after[i];
 
     long long min_operations = GetMinXOROperations(vector_before, vector_after, N);
 
-    std::cout << (min_operations >= 0 ? std::to_string(min_operations) : "IMPOSSIBLE") << std::endl;
+    std::cout << (min_operations >= 0LL ? std::to_string(min_operations) : "IMPOSSIBLE") << std::endl;
 
     return 0;
 }
