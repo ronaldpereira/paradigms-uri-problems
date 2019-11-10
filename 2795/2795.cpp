45ad2ebe208c18da370d443206cd1f8f76674d01
Ronald <ronald.drp11@gmail.com>
2019-11-10 01:04:37 -0300

    2795 correct

--- 2795/2795.cpp
+++ 2795/2795.cpp
@@ -6,39 +6,47 @@
 std::string word;
 std::vector<std::vector<int>> dp;
 std::vector<std::vector<int>> change_cost_dp(26, std::vector<int>(26));
+std::vector<std::vector<int>> palindrome_cost_dp;
 
 int GetChangeCost(int index_begin, int index_end)
 {
-    int cost = 0;
-    for (int i = index_begin, j = index_end; i < j; i++, j--)
-        cost += change_cost_dp[(int)word[i] - 'a'][(int)word[j] - 'a'];
+    if (palindrome_cost_dp[index_begin][index_end] == -1)
+    {
+        int cost = 0;
+        for (int i = index_begin, j = index_end; i < j; i++, j--)
+            cost += change_cost_dp[(int)word[i] - 'a'][(int)word[j] - 'a'];
 
-    return cost;
+        palindrome_cost_dp[index_begin][index_end] = cost;
+    }
+
+    return palindrome_cost_dp[index_begin][index_end];
 }
 
 int PalindromeCost(int K, int i)
 {
     if (K == 1)
     {
-        if (dp[i][K] == -1)
-            dp[i][K] = GetChangeCost(i, word.length() - 1);
-
-        return dp[i][K];
+        if (dp[1][i] == -1)
+        {
+            dp[1][i] = GetChangeCost(i, word.length() - 1);
+        }
+        return dp[1][i];
     }
 
     else
     {
-        int cost = PalindromeCost(1, i);
-        for (int j = i; j < word.length() - 1; j++)
+        if (dp[K][i] == -1)
         {
-            int left = GetChangeCost(i, j);
-            int right = (dp[j + 1][K - 1] == -1) ? PalindromeCost(K - 1, j + 1) : dp[j + 1][K - 1];
-            dp[j + 1][K - 1] = right;
-            cost = std::min(cost, left + right);
+            int cost = PalindromeCost(1, i);
+            for (int j = i; j < word.length() - 1; j++)
+            {
+                int actual_cost = GetChangeCost(i, j);
+                actual_cost += PalindromeCost(K - 1, j + 1);
+                cost = std::min(cost, actual_cost);
+            }
+            dp[K][i] = cost;
         }
-        dp[i][K] = cost;
-
-        return dp[i][K];
+        return dp[K][i];
     }
 }
 
@@ -57,9 +65,14 @@ int main()
         for (int j = 0; j < 26; j++)
             change_cost_dp[i][j] = std::min(std::abs(j - i), 26 - std::abs(j - i));
 
-    dp = std::vector<std::vector<int>>((N), std::vector<int>(K + 1));
+    palindrome_cost_dp = std::vector<std::vector<int>>((N), std::vector<int>(N));
     for (int i = 0; i < N; i++)
-        for (int j = 0; j <= K; j++)
+        for (int j = 0; j < N; j++)
+            palindrome_cost_dp[i][j] = (i == j) ? 0 : -1;
+
+    dp = std::vector<std::vector<int>>((K + 1), std::vector<int>(N));
+    for (int i = 0; i <= K; i++)
+        for (int j = 0; j < N; j++)
             dp[i][j] = -1;
 
     std::cout << PalindromeCost(K, 0) << std::endl;
